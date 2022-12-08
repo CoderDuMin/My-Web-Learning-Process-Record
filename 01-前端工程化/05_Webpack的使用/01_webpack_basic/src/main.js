7379f5828cae88411607fc270b9f59c4c304c0fb
CoderDuMin <1325964835@qq.com>
2022-12-08 20:46:41 +0800

    webpack初步使用

---
+++ 01-前端工程化/05_Webpack的使用/01_webpack_basic/src/main.js
@@ -0,0 +1,4 @@
+import { sum,sub } from './utils/math.js'
+
+console.log(sum(1,4))
+console.log(sub(20,12))
---
+++ 01-前端工程化/05_Webpack的使用/01_webpack_basic/src/utils/math.js
@@ -0,0 +1,12 @@
+function sum(num1,num2){
+  return num1 + num2
+}
+
+function sub(num1,num2){
+  return num1 - num2
+}
+
+module.exports = {
+  sub,
+  sum
+}
