5bd97859ee6ddd8d48c2df00b6975fb593de02a8
CoderDuMin <1325964835@qq.com>
2023-01-18 22:41:59 +0800

    setup函数学习

--- 02_Learn_VueJs/04_learn_composition/src/01_Setup函数的体验/App.vue
+++ 02_Learn_VueJs/04_learn_composition/src/01_Setup函数的体验/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 import { ref } from 'vue'
-import useCounter from './hooks/useCounter'
+import useCounter from '../hooks/useCounter'
 export default {
   setup(){
     // 1.常规写法
--- 02_Learn_VueJs/04_learn_composition/src/main.js
+++ 02_Learn_VueJs/04_learn_composition/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+// import App from './01_Setup函数的体验/App.vue'
+// import App from './02_Setup定义数据/App.vue'
+// import App from './03_setup其他函数/App.vue'
+// import App from './04_Setup中toRefs/App.vue'
+import App from './05_Setup中computed/App.vue'
 
 createApp(App).mount('#app')
