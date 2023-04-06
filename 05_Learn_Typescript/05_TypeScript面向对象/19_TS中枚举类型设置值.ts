37f951b5cf6d6c453cffacd974f354aef1282a44
CoderDuMin <1325964835@qq.com>
2023-04-06 20:05:27 +0800

    TS-枚举类型

---
+++ 05_Learn_Typescript/05_TypeScript面向对象/19_TS中枚举类型设置值.ts
@@ -0,0 +1,24 @@
+enum Direction {
+  LEFT=101,
+  RIGHT,
+  TOP='aaa',
+  BOTTOM='bbb'
+}
+
+const d1: Direction = Direction.LEFT
+
+function turnDirection(direction: Direction) {
+  switch(direction) {
+    case Direction.LEFT:
+      console.log("角色向左移动一个格子")
+      break
+    case Direction.RIGHT:
+      console.log("角色向右移动一个格子")
+      break
+  }
+}
+
+// 监听键盘的点击
+turnDirection(Direction.LEFT)
+
+export {}
